6bd75e6ff4b331d477149452c00f7f4741cba0d7
alexbagirov <a.d.bagirov@gmail.com>
2018-04-24 20:24:09 +0500

    Initial commit

---
+++ TwoThousandsFourtyEight/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace TwoThousandsFourtyEight
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var form = new Form();
+            Application.Run(form);
+        }
+    }
+}
