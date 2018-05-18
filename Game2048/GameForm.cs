42a0de237b1c2d7fb869b1db65a8ad83cba93728
DimaIvanovskiy <dima.ivanovskiy.99@mail.ru>
2018-05-18 21:26:52 +0500

    Adds lables

--- Game2048/GameForm.cs
+++ Game2048/GameForm.cs
@@ -8,14 +8,14 @@ namespace Game2048
         public GameForm(int width, int height)
         {
             var game = new Game(width, height);
-            
+
             Size = Screen.PrimaryScreen.WorkingArea.Size;
             MinimumSize = Size;
             MaximumSize = Size;
             BackColor = ColorTranslator.FromHtml("#faf8ef");
             KeyPreview = true;
 
-            var table = new TableLayoutPanel {Dock = DockStyle.Fill};
+            var table = new TableLayoutPanel { Dock = DockStyle.Fill };
             table.RowStyles.Add(new RowStyle(SizeType.Percent, 35));
             table.RowStyles.Add(new RowStyle(SizeType.Absolute, Screen.PrimaryScreen.WorkingArea.Width / 3 + 20));
             table.RowStyles.Add(new RowStyle(SizeType.Percent, 15));
@@ -23,58 +23,58 @@ namespace Game2048
             table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
             table.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, Screen.PrimaryScreen.WorkingArea.Width / 3 + 20));
             table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
-            
-            table.Controls.Add(new Panel{Dock = DockStyle.Fill}, 0, 0);
-            table.Controls.Add(new Panel{Dock = DockStyle.Fill}, 0, 1);
-            table.Controls.Add(new Panel{Dock = DockStyle.Fill}, 0, 2);
-            table.Controls.Add(new Panel{Dock = DockStyle.Fill}, 1, 2);
-            table.Controls.Add(new Panel{Dock = DockStyle.Fill}, 2, 0);
-            table.Controls.Add(new Panel{Dock = DockStyle.Fill}, 2, 1);
-            table.Controls.Add(new Panel{Dock = DockStyle.Fill}, 2, 2);
 
-            var head = new TableLayoutPanel {Dock = DockStyle.Fill};
-            
+            table.Controls.Add(new Panel { Dock = DockStyle.Fill }, 0, 0);
+            table.Controls.Add(new Panel { Dock = DockStyle.Fill }, 0, 1);
+            table.Controls.Add(new Panel { Dock = DockStyle.Fill }, 0, 2);
+            table.Controls.Add(new Panel { Dock = DockStyle.Fill }, 1, 2);
+            table.Controls.Add(new Panel { Dock = DockStyle.Fill }, 2, 0);
+            table.Controls.Add(new Panel { Dock = DockStyle.Fill }, 2, 1);
+            table.Controls.Add(new Panel { Dock = DockStyle.Fill }, 2, 2);
+
+            var head = new TableLayoutPanel { Dock = DockStyle.Fill };
+
             head.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
             head.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
-            
+
             head.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
             head.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-            
+
             head.Controls.Add(new Label
             {
-                Text = "2048", Dock = DockStyle.Fill, 
-                Font = new Font("Arial", 50, FontStyle.Bold), 
+                Text = "2048", Dock = DockStyle.Fill,
+                Font = new Font("Arial", 50, FontStyle.Bold),
                 ForeColor = ColorTranslator.FromHtml("#776e65"),
                 TextAlign = ContentAlignment.MiddleLeft
             }, 0, 0);
             head.Controls.Add(new Label
             {
                 Text = "0", ForeColor = ColorTranslator.FromHtml("#776e65"),
-                Font = new Font("Arial", 18, FontStyle.Bold), 
-                TextAlign = ContentAlignment.MiddleRight, 
+                Font = new Font("Arial", 18, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleRight,
                 Dock = DockStyle.Fill
             }, 1, 0);
             head.Controls.Add(new Label
             {
                 Text = "Join the numbers and get to the 2048 tile!",
-                Font = new Font("Arial", 15, FontStyle.Bold), 
+                Font = new Font("Arial", 15, FontStyle.Bold),
                 ForeColor = ColorTranslator.FromHtml("#776e65"),
                 TextAlign = ContentAlignment.MiddleLeft,
                 Dock = DockStyle.Fill
             }, 0, 1);
             head.Controls.Add(new Button
             {
-                Text = "New Game", 
+                Text = "New Game",
                 Font = new Font("Arial", 13, FontStyle.Bold),
                 ForeColor = Color.White,
                 BackColor = ColorTranslator.FromHtml("#8f7a66"),
-                FlatStyle = FlatStyle.Flat, 
-                FlatAppearance = {BorderSize = 0}, 
+                FlatStyle = FlatStyle.Flat,
+                FlatAppearance = { BorderSize = 0 },
                 Anchor = AnchorStyles.Right,
-                AutoSize = true, 
+                AutoSize = true,
                 Padding = new Padding(4)
             }, 1, 1);
-            
+
             table.Controls.Add(head, 1, 0);
 
             var gameField = new GameField();
@@ -86,21 +86,21 @@ namespace Game2048
             
             Controls.Add(table);
 
-            var panels = StartGame(game, gameField, head);
+            var labels = StartGame(game, gameField, head);
 
-            KeyDown += (sender, args) => MakeMove(game, panels, head, args.KeyData);
+            KeyDown += (sender, args) => MakeMove(game, labels, head, args.KeyData);
             head.Controls[3].Click += (sender, args) =>
             {
                 game = new Game(4, 4);
                 gameField = new GameField();
                 StartGame(game, gameField, head);
-                UpdateColors(game, panels);
+                UpdateColors(game, labels);
             };
         }
 
         private Control[,] StartGame(Game game, TableLayoutPanel field, TableLayoutPanel head)
         {
-            var panels = new Control[game.Width, game.Height];
+            var labels = new Control[game.Width, game.Height];
             field.Controls.Clear();
             head.Controls[1].Text = "0";
             
@@ -108,26 +108,36 @@ namespace Game2048
             {
                 for (var j = 0; j < game.Width; j++)
                 {
-                    panels[j, i] = new Panel {Dock = DockStyle.Fill, BackColor = game[j, i].Color};
-                    field.Controls.Add(panels[j, i], j, i);
+                    labels[j, i] = new Label
+                    {
+                        Size = new Size(50, 50),
+                        Dock = DockStyle.Fill,
+                        BackColor = game[j, i].Color,
+                        Text = game[j, i].Value == 0 ? "" : game[j, i].Value.ToString(),
+                        Font = new Font("Arial", 30, FontStyle.Bold),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        ForeColor = ColorTranslator.FromHtml("#776e65"),
+                    };
+                    field.Controls.Add(labels[j, i], j, i);
                 }
             }
-            return panels;
+            return labels;
         }
 
-        private void UpdateColors(Game game, Control[,] panels)
+        private void UpdateColors(Game game, Control[,] labels)
         {
             for (var i = 0; i < game.Height; i++)
             {
                 for (var j = 0; j < game.Width; j++)
                 {
-                    panels[j, i].BackColor = game[j, i].Color;
+                    labels[j, i].Text = game[j, i].Value == 0 ? "" : game[j, i].Value.ToString();
+                    labels[j, i].BackColor = game[j, i].Color;
                 }
             }
             Invalidate();
         }
 
-        private void MakeMove(Game game, Control[,] panels, TableLayoutPanel head, Keys key)
+        private void MakeMove(Game game, Control[,] labels, TableLayoutPanel head, Keys key)
         {
             var moved = false;
             switch (key)
@@ -162,7 +172,7 @@ namespace Game2048
                 head.Controls[1].Text = game.Score.ToString();
             }
                 
-            UpdateColors(game, panels);
+            UpdateColors(game, labels);
         }
     }
 
